8053dca520ee338a276836b22e8f8edac7a6d307
Sankalp Sharma <sankalp.sharma156@gmail.com>
2023-07-31 18:49:39 +0530

    blog link added fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
-              href='blog-bfw3r5gbg-sankalpsharmaofficial.vercel.app'
+              href='https://blog-bfw3r5gbg-sankalpsharmaofficial.vercel.app/'
               target="_blank"
               rel="noreferrer"
             >
