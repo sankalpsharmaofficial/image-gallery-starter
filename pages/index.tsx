0fb0ec0868c82577d6df4f3016fc745d427dff61
Sankalp Sharma <sankalp.sharma156@gmail.com>
2023-09-10 13:40:00 +0530

    shadcn-avatar size reduced to 5

--- pages/index.tsx
+++ pages/index.tsx
@@ -102,8 +102,6 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         </div>
       </main>
       <footer className="p-6 text-center text-white/80 sm:p-12">
-      
-
         Photography by{' '}
         <a
           href="https://sankalpsharma.vercel.app/"
@@ -113,7 +111,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         >
           Sankalp Sharma
         </a>
-        <Avatar className='w-10 h-10 rounded-full'>
+        <Avatar className='w-5 h-5 rounded-full'>
           <AvatarImage src="https://github.com/sankalpsharmaofficial.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
